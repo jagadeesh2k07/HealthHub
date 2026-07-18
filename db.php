b8116e0f508c8aeaf837d7c0cd8214a989949fb0
K Jagadeesh <jagadeesh2k07@gmail.com>
2026-07-19 00:04:11 +0530

    Updated the db data

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host     = "localhost";
-$user     = "root";
-$password = "";
-$database = "healthhub_db";
+$host     = "sql313.infinityfree.com";
+$user     = "if0_42297065";
+$password = "4L1uhbr1hk5";
+$database = "if0_42297065_healthhub";
 $conn = mysqli_connect($host, $user, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
